b1ee61451e2a173bfbc0018b7b16c6f6c401a334
Henny Worabai <h.hennylianeadolineaksamine@students.uu.nl>
2026-06-09 15:07:21 +0200

    Add "Bye"

--- fortune-cookie.py
+++ fortune-cookie.py
@@ -57,4 +57,4 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("\nCome back for more anytime!")
+        print("\nCome back for more anytime! Bye")
